3cdb3406082a7aeac02bd24ac001d0523146650a
Your Name <you@example.com>
2023-04-04 20:05:54 -0500

    intital item abs

---
+++ src/inventory/__init__.py
@@ -0,0 +1,9 @@
+"""Meta package for all project packages.
+This is the __init__ file for this package.
+Typically this file can be left blank, but for this example we have
+included a print statement so we can see what it does and when.
+Usage:
+    python3 -m src - execute this program (when run from project root).
+Author: John Jalali JJalali@ksu.edu
+Version: 0.1
+"""
--- src/inventory/data/Item.py
+++ src/inventory/data/Item.py
@@ -1,4 +1,67 @@
+"""Main interface for the created items.
 
-class Item:
-    def __init__(self):
-        pass
+Author: John Jalali JJalali@ksu.edu
+Version: 0.1
+TODO: Tentative Feels like a lot of abstract methods.
+"""
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Item(ABC):
+    """Impliments getters for shared class attribues."""
+    @classmethod
+    def __subclasshook__(cls, subclass: type) -> bool:
+        """Checks class and subclass"""
+        if cls is Item:
+            callables: List[str] = ["name", "stock_num", "qty", "locaiton",
+                                   "priority", "cost", "restock_estimate"]
+            ret = True
+            for call in callables:
+                ret = ret and (hasattr(subclass, call) and 
+                               callable(getattr(subclass, call)))
+            return ret
+        else:
+            return NotImplemented
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        """Getter for Name."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def stock_num(self) -> str:
+        """Getter for stock number."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def quantity(self) -> str:
+        """Getter for quantity."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def location(self) -> str:
+        """Getter for location."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def priority(self) -> str:
+        """Getter for priority."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def cost(self) -> str:
+        """Getter for cost."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def restock_estimate(self) -> str:
+        """Getter for restock estimate."""
+        raise NotImplementedError
